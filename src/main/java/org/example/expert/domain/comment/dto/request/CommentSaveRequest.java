97d6a3a21db34da10542d8c1d19d93bfc49aa5b1
bbobsang <bbobsang@naver.com>
2024-11-18 15:19:01 +0900

    [과제 2-2] feat: 코드 구조 개선

--- src/main/java/org/example/expert/domain/comment/dto/request/CommentSaveRequest.java
+++ src/main/java/org/example/expert/domain/comment/dto/request/CommentSaveRequest.java
@@ -1,15 +1,17 @@
 package org.example.expert.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class CommentSaveRequest {
 
-    @NotBlank
-    private String contents;
+    @NotBlank(message = "Comment content is required")
+    private String content;
+
+    public CommentSaveRequest(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/org/example/expert/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/org/example/expert/domain/comment/dto/response/CommentResponse.java
@@ -1,18 +1,26 @@
 package org.example.expert.domain.comment.dto.response;
 
 import lombok.Getter;
-import org.example.expert.domain.user.dto.response.UserResponse;
 
-@Getter
 public class CommentResponse {
 
-    private final Long id;
-    private final String contents;
-    private final UserResponse user;
+    @Getter
+    private Long id;
 
-    public CommentResponse(Long id, String contents, UserResponse user) {
+    @Getter
+    private String content;
+
+    @Getter
+    private Long todoId;
+
+
+    public CommentResponse(Long id,
+                           String content,
+                           Long todoId) {
         this.id = id;
-        this.contents = contents;
-        this.user = user;
+        this.content = content;
+        this.todoId = todoId;
+
     }
+
 }
