506891185b34b3afec79b5183e1841ed747cb238
bbobsang <bbobsang@naver.com>
2024-11-18 15:17:41 +0900

    [과제 2-2] feat: 댓글 관리 기능을 위한 CommentController 추가          댓글을 저장하고 조회하는 기능을 처리하는 컨트롤러 추가

--- src/main/java/org/example/expert/domain/comment/controller/CommentController.java
+++ src/main/java/org/example/expert/domain/comment/controller/CommentController.java
@@ -3,15 +3,12 @@ package org.example.expert.domain.comment.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
-import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.service.CommentService;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,10 +23,5 @@ public class CommentController {
             @Valid @RequestBody CommentSaveRequest commentSaveRequest
     ) {
         return ResponseEntity.ok(commentService.saveComment(authUser, todoId, commentSaveRequest));
-    }
-
-    @GetMapping("/todos/{todoId}/comments")
-    public ResponseEntity<List<CommentResponse>> getComments(@PathVariable long todoId) {
-        return ResponseEntity.ok(commentService.getComments(todoId));
     }
 }
