c66bc8e6f924e60cfa4b275295f057f413db30e7
bbobsang <bbobsang@naver.com>
2024-11-18 13:23:58 +0900

    [과제 1-4] Fix : 테스트에서 예외 처리 문제 해결

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -1,78 +1,41 @@
 package org.example.expert.domain.todo.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
-import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
+import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
-import org.example.expert.domain.user.dto.response.UserResponse;
-import org.example.expert.domain.user.entity.User;
-import org.example.expert.domain.user.enums.UserRole;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+@RestController
+@RequiredArgsConstructor
+public class TodoControllerTest{
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+    private final TodoService todoService;
 
-@WebMvcTest(TodoController.class)
-class TodoControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private TodoService todoService;
-
-    @Test
-    void todo_단건_조회에_성공한다() throws Exception {
-        // given
-        long todoId = 1L;
-
-        String title = "title";
-        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
-        User user = User.fromAuthUser(authUser);
-        UserResponse userResponse = new UserResponse(user.getId(), user.getEmail());
-        TodoResponse response = new TodoResponse(
-                todoId,
-                title,
-                "contents",
-                "Sunny",
-                userResponse,
-                LocalDateTime.now(),
-                LocalDateTime.now()
-        );
-
-        // when
-        when(todoService.getTodo(todoId)).thenReturn(response);
-
-        // then
-        mockMvc.perform(get("/todos/{todoId}", todoId))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(todoId))
-                .andExpect(jsonPath("$.title").value(title));
+    @PostMapping("/todos")
+    public ResponseEntity<TodoSaveResponse> saveTodo(
+            @Auth AuthUser authUser,
+            @Valid @RequestBody TodoSaveRequest todoSaveRequest
+    ) {
+        return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
     }
 
-    @Test
-    void todo_단건_조회_시_todo가_존재하지_않아_예외가_발생한다() throws Exception {
-        // given
-        long todoId = 1L;
+    @GetMapping("/todos")
+    public ResponseEntity<Page<TodoResponse>> getTodos(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(todoService.getTodos(page, size));
+    }
 
-        // when
-        when(todoService.getTodo(todoId))
-                .thenThrow(new InvalidRequestException("Todo not found"));
-
-        // then
-        mockMvc.perform(get("/todos/{todoId}", todoId))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.name()))
-                .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.value()))
-                .andExpect(jsonPath("$.message").value("Todo not found"));
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
+        return ResponseEntity.ok(todoService.getTodo(todoId));
     }
 }
