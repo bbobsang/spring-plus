140633032158132d41160431ac8fde842c43325a
bbobsang <bbobsang@naver.com>
2024-11-18 14:06:50 +0900

    [과제 1-5] feat: `getTodos` API에 날씨와 기간 필터링 조건을 위한 파라미터 추가

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -29,9 +31,12 @@ public class TodoController {
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String weather,        // 날씨 조건 (optional)
+            @RequestParam(required = false) LocalDateTime startDate, // 기간 시작일 (optional)
+            @RequestParam(required = false) LocalDateTime endDate   // 기간 종료일 (optional)
     ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+        return ResponseEntity.ok(todoService.getTodos(page, size, weather, startDate, endDate));
     }
 
     @GetMapping("/todos/{todoId}")
